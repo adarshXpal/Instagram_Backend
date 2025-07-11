cb43a07483b89f3d3d585907a0a47cd739498093
adarshXpal <protoyoru07@gmail.com>
2025-07-11 23:03:39 +0530

    .env remove

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
