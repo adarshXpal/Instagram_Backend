1162c12c5293405440d501874d26c3d1d9aa00c2
adarshXpal <protoyoru07@gmail.com>
2025-07-12 00:47:32 +0530

    Redis Service added

---
+++ src/utils/Redis.ts
@@ -0,0 +1,44 @@
+import Redis from "ioredis";
+
+class RedisService {
+    private redis: Redis;
+
+    constructor(redisHost: string, redisPort: number) {
+        if (
+            redisHost.startsWith("redis://") ||
+            redisHost.startsWith("rediss://")
+        ) {
+            this.redis = new Redis(redisHost);
+        } else {
+            this.redis = new Redis({
+                host: redisHost,
+                port: redisPort,
+            });
+        }
+
+        this.redis.on("connect", () => {
+            console.log("Redis client connected");
+        });
+        this.redis.on("error", (err) => {
+            console.error("Redis error:", err);
+        });
+    }
+
+    async get(key: string): Promise<string | null> {
+        return this.redis.get(key);
+    }
+
+    async set(key: string, value: string, ttl?: number): Promise<void> {
+        if (ttl) {
+            await this.redis.set(key, value, "EX", ttl);
+        } else {
+            await this.redis.set(key, value);
+        }
+    }
+
+    async del(key: string): Promise<void> {
+        this.redis.del(key);
+    }
+}
+
+export default RedisService;
