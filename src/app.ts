d88df4ae1ea7de07594971508578a2f9144d9590
adarshXpal <protoyoru07@gmail.com>
2025-07-11 23:53:06 +0530

    routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import Routes from "routes";
 
 const app = express();
 
@@ -9,10 +10,18 @@ app.use(helmet());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
+
+app.use("/api/auth", Routes.authRoute);
+app.use("/api/users", Routes.userRoute);
+app.use("/api/posts", Routes.postRouter);
+app.use("/api/posts", Routes.postCommentRouter);
+app.use("/api/comments", Routes.commentRouter);
+app.use("/api/stories", Routes.storiesRouter);
+app.use("/api/media", Routes.mediaRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import authRoute from "./auth.routes";
+import userRoute from "./users.routes";
+import postRouter from "./posts.routes";
+import { postCommentRouter, commentRouter } from "./comments.routes";
+import storiesRouter from "./stories.routes";
+import mediaRouter from "./media.routes";
+
+export default {
+    authRoute,
+    userRoute,
+    postRouter,
+    postCommentRouter,
+    commentRouter,
+    storiesRouter,
+    mediaRouter,
+};
