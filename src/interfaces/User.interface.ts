b32ad8f3a9a30db81d86c954a95338fde124a98f
Adarsh <protoyoru07@gmail.com>
2025-07-13 01:04:23 +0530

    user schma changed git add .

--- src/interfaces/User.interface.ts
+++ src/interfaces/User.interface.ts
@@ -2,20 +2,21 @@
 import { Document, ObjectId } from "mongoose";
 
 export interface IUser extends Document {
-    _id: ObjectId;
-    username: string;
-    email: string;
-    password: string;
-    fullName: string;
-    bio?: string;
-    profilePicture?: string;
-    isVerified: boolean;
-    isPrivate: boolean;
-    followers: ObjectId[];
-    following: ObjectId[];
-    followerCount: number;
-    followingCount: number;
-    postCount: number;
-    createdAt: Date;
-    updatedAt: Date;
+  _id: ObjectId;
+  username: string;
+  email: string;
+  password: string;
+  fullName: string;
+  bio?: string;
+  profilePicture?: string;
+  isVerified: boolean;
+  isPrivate: boolean;
+  followers: ObjectId[];
+  following: ObjectId[];
+  followerCount: number;
+  followRequests: ObjectId[];
+  followingCount: number;
+  postCount: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -2,22 +2,23 @@ import { IUser } from "interfaces/User.interface";
 import { model, Schema } from "mongoose";
 
 //Mongoose Schema
-export const userSchema = new Schema<IUser>(
-    {
-        username: { type: String, required: true, unique: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        fullName: { type: String, required: true },
-        bio: { type: String },
-        profilePicture: { type: String },
-        isVerified: { type: Boolean, default: false },
-        isPrivate: { type: Boolean, default: false },
-        followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
-        following: [{ type: Schema.Types.ObjectId, ref: "User" }],
-        followerCount: { type: Number, default: 0 },
-        followingCount: { type: Number, default: 0 },
-        postCount: { type: Number, default: 0 },
-    },
-    { timestamps: true }
+const userSchema = new Schema<IUser>(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    fullName: { type: String, required: true },
+    bio: { type: String },
+    profilePicture: { type: String },
+    isVerified: { type: Boolean, default: false },
+    isPrivate: { type: Boolean, default: false },
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followerCount: { type: Number, default: 0 },
+    followingCount: { type: Number, default: 0 },
+    postCount: { type: Number, default: 0 },
+  },
+  { timestamps: true }
 );
 export default model<IUser>("users", userSchema);
