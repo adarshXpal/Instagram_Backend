f0ab55f65d2f44cd09414c4761a165bc4744ddd1
adarshXpal <protoyoru07@gmail.com>
2025-07-13 01:07:16 +0530

    create post done

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,4 +1,31 @@
-export const createPosts = () => {};
+import DatabaseService from "../utils/database";
+import ResponseService from "../utils/response.utils";
+import PostModel from "../models/Post.model";
+
+export const createPosts = async (req: Request, res: Response) => {
+    const { caption, imageUrl, location, tags = [] } = req.body;
+
+    if (!imageUrl) {
+        return ResponseService.error(res, "image url is require", 400, {});
+    }
+
+    try {
+        const db = new DatabaseService(PostModel);
+        const body = { imageUrl, tags, user: req.user.id };
+        if (caption) {
+            body.caption = caption;
+        }
+
+        if (location) {
+            body.location = location;
+        }
+
+        const data = await db.create(body);
+        ResponseService.success(res, data, "Post created Successfully", 201);
+    } catch (err) {
+        ResponseService.error(res, "Something went wrong", 500, err);
+    }
+};
 export const getPosts = () => {};
 export const getPostById = () => {};
 export const updatePostById = () => {};
