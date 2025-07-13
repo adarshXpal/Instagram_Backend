ec7b844581e79cd53c6108e5fd35e0715fab5f3d
adarshXpal <protoyoru07@gmail.com>
2025-07-13 23:52:17 +0530

    media upload done

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,18 @@
 import authRoute from "./auth.routes";
 import userRoute from "./users.routes";
 import postRouter from "./posts.routes";
-import commentRouter from "./comments.routes"
+import commentRouter from "./comments.routes";
 // import { postCommentRouter, commentRouter } from "./comments.routes";
 // import storiesRouter from "./stories.routes";
-// import mediaRouter from "./media.routes";
+import mediaRouter from "./media.routes";
 
 export default {
-  authRoute,
-  userRoute,
-  postRouter,
-  commentRouter,
-  // postCommentRouter,
-  // commentRouter,
-  // storiesRouter,
-  // mediaRouter,
+    authRoute,
+    userRoute,
+    postRouter,
+    commentRouter,
+    // postCommentRouter,
+    // commentRouter,
+    // storiesRouter,
+    mediaRouter,
 };
